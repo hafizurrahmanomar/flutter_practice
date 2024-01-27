53cf363464ee919446895ee978a60de96fbf7f4c
Hafizur Rahman Omar <hafizurrahmanomar@gmail.com>
2024-01-27 19:05:30 +0600

    Update todo_list_screen.dart

--- module_12_class_1_todoapp_design/lib/todo_list_screen.dart
+++ module_12_class_1_todoapp_design/lib/todo_list_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:helloworld/add_new_todo_screen.dart';
 
 class TodoListScreen extends StatefulWidget {
-  const TodoListScreen({super.key});
+  const TodoListScreen({Key? key}) : super(key: key);
 
   @override
   State<TodoListScreen> createState() => _TodoListScreenState();
@@ -10,12 +11,50 @@ class TodoListScreen extends StatefulWidget {
 class _TodoListScreenState extends State<TodoListScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(appBar: AppBar(
-      centerTitle: true,
-      backgroundColor: Colors.deepPurpleAccent,
-      title: const Text("Todo App",style: TextStyle(
-      color: Colors.white
-    ),),),);
- }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Todo List",
+        ),
+      ),
+      body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: const Text("To Do List"),
+              subtitle: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("To Do Body"),
+                  Text("time"),
+                ],
+              ),
+              trailing: Wrap(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.delete_outline_outlined),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.edit),
+                  ),
+                ],
+              ),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.deepOrange,
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const AddNewTodoScreen(),
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
 }
-
