80fe7945c7f956968d4e6582dd57bf4529eafc55
Hafizur Rahman Omar <hafizurrahmanomar@gmail.com>
2024-01-27 19:04:52 +0600

    Update todo_app.dart

--- module_12_class_1_todoapp_design/lib/todo_app.dart
+++ module_12_class_1_todoapp_design/lib/todo_app.dart
@@ -1,13 +1,78 @@
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:module_12_class_1_todoapp_design/todo_list_screen.dart';
+import 'package:helloworld/todo_list_screen.dart';
 
-class ToDoApp  extends StatelessWidget{
-  const ToDoApp();
+class TodoApp extends StatelessWidget {
+  const TodoApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return const MaterialApp(home: TodoListScreen(),);
+    /// MaterialApp Section Start
+    return MaterialApp(
+      home: TodoListScreen(),
+
+      ///ThemeData Section Start
+
+      theme: ThemeData(
+          colorSchemeSeed: Colors.blue,
+
+          /// AppBarTheme Section Start
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.blue,
+            centerTitle: true,
+            iconTheme: IconThemeData(color: Colors.amberAccent),
+            titleTextStyle: TextStyle(
+                color: Colors.white, fontSize: 35, fontWeight: FontWeight.bold),
+          ),
+
+          /// AppBarTheme Section End
+
+          ///TextFormField in Theme data Start
+          inputDecorationTheme: const InputDecorationTheme(
+            border: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.blue),
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+            disabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.amber),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.red),
+            ),
+          ),
+
+          ///TextFormField in Themedata End
+
+          ////// ElevatedButton custom Design Start
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.indigoAccent,
+              foregroundColor: Colors.white,
+              shadowColor: Colors.grey,
+              elevation: 7.0,
+              padding: EdgeInsets.all(20.00),
+              textStyle: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic),
+              minimumSize: Size(50, 30),
+            ),
+          )
+
+          ////// ElevatedButton custom Design End
+          ),
+
+      ///ThemeData Section End
+    );
+
+    /// MaterialApp Section End
   }
 }
